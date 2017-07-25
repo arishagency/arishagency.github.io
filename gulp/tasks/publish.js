16ec4b89bac7e3093ebaea15047734202953db49
Nicholas Frota <info@nicholasfrota.com>
2017-07-25 17:52:29 -0400

    publish

---
+++ gulp/tasks/publish.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+runSequence = require('run-sequence'),
+ghPages = require('gulp-gh-pages')
+    fs = require("fs"),
+    globalVar = JSON.parse(fs.readFileSync("./site.json", "utf8"));
+
+// moves dist to gh-pages
+
+gulp.task('publish', function() {
+  return gulp.src(globalVar.distFolder + '**/*')
+  .pipe(ghPages());
+});
