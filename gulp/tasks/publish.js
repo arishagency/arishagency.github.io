90df6027468d3e19c051b46e85406eb9428b507c
Nicholas Frota <info@nicholasfrota.com>
2017-07-26 11:15:04 -0400

    fixing gulp publish

--- gulp/tasks/publish.js
+++ gulp/tasks/publish.js
@@ -1,12 +1,12 @@
-var gulp = require('gulp'),
-runSequence = require('run-sequence'),
-ghPages = require('gulp-gh-pages')
+var gulp = require("gulp"),
+runSequence = require("run-sequence"),
+ghPages = require("gulp-gh-pages")
     fs = require("fs"),
     globalVar = JSON.parse(fs.readFileSync("./site.json", "utf8"));
 
 // moves dist to gh-pages
 
-gulp.task('publish', function() {
-  return gulp.src(globalVar.distFolder + '**/*')
+gulp.task("publish", function() {
+  return gulp.src(globalVar.distFolder + "/**/*")
   .pipe(ghPages());
 });
