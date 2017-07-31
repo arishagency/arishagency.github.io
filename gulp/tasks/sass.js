dadd1f99c0bce2173da24c17aca7dd9c1dc1db21
Nicholas Frota <info@nicholasfrota.com>
2017-07-31 19:01:36 -0400

    error handling for SASS

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,10 +3,12 @@
 var gulp = require("gulp"),
     sass = require("gulp-sass"),
     fs = require("fs"),
+    customPlumber = require("../lib/plumber"),
     globalVar = JSON.parse(fs.readFileSync("./site.json", "utf8"));
 
 gulp.task("sass", function () {
     return gulp.src(globalVar.editFolder + "/sass/**/style.scss")
+.pipe(customPlumber('SCSS Error'))
         .pipe(sass())
         .pipe(gulp.dest(globalVar.distFolder + "/css"));
 
