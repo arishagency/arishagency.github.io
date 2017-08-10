f679a97a9d975ac45ca1bb6612cc108df2e7f8a9
Nicholas Frota <info@nicholasfrota.com>
2017-08-09 21:24:36 -0400

    clean

--- gulp/tasks/clean.js
+++ gulp/tasks/clean.js
@@ -6,10 +6,5 @@ var gulp = require("gulp"),
     globalVar = JSON.parse(fs.readFileSync("./site.json", "utf8"));
 
 gulp.task("clean", function () {
-    return removeDir([
-        globalVar.distFolder + "/css",
-        globalVar.distFolder + "/fonts",
-        globalVar.distFolder + "/images",
-        globalVar.distFolder + "/js"
-        ]);
+    return removeDir(globalVar.distFolder);
 });
