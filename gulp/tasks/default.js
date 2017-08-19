f253b0d76da23e8f625735431cd55b0b6dc4d424
Attila <attila.gulyas.kovacs@gmail.com>
2017-08-19 16:08:26 -0400

    gulp default task starts with a clean of dist/

--- gulp/tasks/default.js
+++ gulp/tasks/default.js
@@ -4,7 +4,7 @@
 var gulp = require("gulp"),
     runSequence = require("run-sequence");
 
-gulp.task("default", function(done) {
+gulp.task("default", ["clean"], function(done) {
     process.env.GSD_PUBLISHED = "true";
     runSequence(["build", "develop"], done);
 });
